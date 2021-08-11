615e09b70e66af25a8de06c7679ec4b5bfdb4660
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-11 10:19:28 -0700

    Split run commands

--- reboundx/implementation/test/multipbs.py
+++ reboundx/implementation/test/multipbs.py
@@ -6,15 +6,12 @@ aus = np.arange(1, 1.2, 0.1) # in AU
 with open('processes.txt', 'w') as f:
     for au in aus:
         rc = ['qsub',
-              '-j oe',
-              '-l select=1:ncpus=1',
-              '-l walltime=0:01:00',
-              '-m abe',
-              '-N test_{:.1f}au'.format(au),
-              '--',
-              'args.py',
-              '{:.1f}'.format(au),
-              ]
+              '-j', 'oe',
+              '-l', 'select=1:ncpus=1',
+              '-l', 'walltime=0:01:00',
+              '-m', 'abe',
+              '-N', 'test_{:.1f}au'.format(au),
+              '--', 'args.py', '{:.1f}'.format(au)]
         complete = subprocess.run(rc)
         f.write('args = %s\n'%(complete.args))
         f.write('  returncode = %s\n'%(complete.returncode))
