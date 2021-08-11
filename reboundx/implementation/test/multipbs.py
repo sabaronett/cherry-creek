a668c73eafd6e674d3869e28eda1ed7c59c64367
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-11 16:26:58 -0700

    Correct Python path

--- reboundx/implementation/test/multipbs.py
+++ reboundx/implementation/test/multipbs.py
@@ -12,7 +12,7 @@ with open('processes.txt', 'w') as f:
               '-m', 'abe',
               '-N', 'test_{:.1f}au'.format(au),
               '-q', 'small',
-              '-S', '~/.conda/envs/rebx-3.4.1/bin/python',
+              '-S', '/home/barons2/.conda/envs/rebx-3.4.1/bin/python',
               '--', 'args.py', '{:.1f}'.format(au)]
         complete = subprocess.run(rc)
         f.write('args = %s\n'%(complete.args))
