0277cd27d0e1181c4d6d275e917baa805f410570
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-11 19:15:53 -0700

    Set interpreter path and pwd

--- reboundx/implementation/test/multipbs.py
+++ reboundx/implementation/test/multipbs.py
@@ -1,6 +1,7 @@
 import subprocess
 import numpy as np
 
+pwd = '/home/barons2/github/sabaronett/cherry-creek/reboundx/implementation/test'
 aus = np.arange(1, 1.2, 0.1) # in AU
 
 with open('processes.txt', 'w') as f:
@@ -13,7 +14,8 @@ with open('processes.txt', 'w') as f:
               '-M', 'barons2@unlv.nevada.edu',
               '-N', 'test_{:.1f}au'.format(au),
               '-q', 'small',
-              '--', 'python', '$PBS_O_WORKDIR/args.py', '{:.1f}'.format(au)]
+              '-S', '/home/barons2/.conda/envs/rebx-3.4.1/bin/python'
+              '--', 'python', pwd+'/args.py', '{:.1f}'.format(au)]
         complete = subprocess.run(rc)
         f.write('args = %s\n'%(complete.args))
         f.write('  returncode = %s\n'%(complete.returncode))
