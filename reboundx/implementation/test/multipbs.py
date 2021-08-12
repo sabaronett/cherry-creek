b5b686f076700413ef5382f251b03b8af64468af
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-11 18:55:22 -0700

    Fix mem req; add email; add pwd

--- reboundx/implementation/test/multipbs.py
+++ reboundx/implementation/test/multipbs.py
@@ -1,19 +1,21 @@
 import subprocess
 import numpy as np
 
+pwd = '/home/barons2/github/sabaronett/cherry-creek/reboundx/implementation/test/'
 aus = np.arange(1, 1.2, 0.1) # in AU
 
 with open('processes.txt', 'w') as f:
     for au in aus:
         rc = ['qsub',
               '-j', 'oe',
-              '-l', 'select=1:ncpus=1',
+              '-l', 'select=1:ncpus=1:mem=1gb',
               '-l', 'walltime=0:01:00',
               '-m', 'abe',
+              '-M', 'barons2@unlv.nevada.edu',
               '-N', 'test_{:.1f}au'.format(au),
               '-q', 'small',
               '-S', '/home/barons2/.conda/envs/rebx-3.4.1/bin/python',
-              '--', 'args.py', '{:.1f}'.format(au)]
+              '--', pwd+'args.py', '{:.1f}'.format(au)]
         complete = subprocess.run(rc)
         f.write('args = %s\n'%(complete.args))
         f.write('  returncode = %s\n'%(complete.returncode))
