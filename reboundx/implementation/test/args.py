548b9365c5b4b4a73735dee6dd11d014c351c324
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-11 19:11:52 -0700

    Fix format code

--- reboundx/implementation/test/args.py
+++ reboundx/implementation/test/args.py
@@ -3,5 +3,5 @@ import sys
 arg1 = sys.argv[1]
 
 print('argv[1] =', arg1)
-with open('{:.1f}au.txt'.format(arg1), 'w') as f:
+with open('{:s}au.txt'.format(arg1), 'w') as f:
     f.write('args = %s\n'%(arg1))
