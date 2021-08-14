667c0cfe585bd7266a72c1ef3b751920311d203d
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-14 15:26:28 -0700

    Make executable path explicit

--- reboundx/implementation/test/multipbs.py
+++ reboundx/implementation/test/multipbs.py
@@ -1,22 +1,17 @@
 import subprocess
 import numpy as np
 
-# pwd = '/home/barons2/github/sabaronett/cherry-creek/reboundx/implementation/test'
+pwd = '/home/barons2/github/sabaronett/cherry-creek/reboundx/implementation/test'
 aus = np.arange(1, 1.2, 0.1) # in AU
 
-with open('processes.txt', 'w') as f:
-    for au in aus:
-        rc = ['qsub',
-              '-j', 'oe',
-              '-l', 'select=1:ncpus=1:mem=1gb',
-              '-l', 'walltime=0:01:00',
-              '-m', 'abe',
-              '-M', 'barons2@unlv.nevada.edu',
-              '-N', 'test_{:.1f}au'.format(au),
-              '-q', 'small',
-              '--', './test.sh', '{:.1f}'.format(au)]
-        complete = subprocess.run(rc)
-        f.write('args = %s\n'%(complete.args))
-        f.write('  returncode = %s\n'%(complete.returncode))
-        f.write('  stdout = %s\n'%(complete.stdout))
-        f.write('  stderr = %s\n\n'%(complete.stderr))
+for au in aus:
+    rc = ['qsub',
+            '-j', 'oe',
+            '-l', 'select=1:ncpus=1:mem=1gb',
+            '-l', 'walltime=0:01:00',
+            '-m', 'abe',
+            '-M', 'barons2@unlv.nevada.edu',
+            '-N', 'test_{:.1f}au'.format(au),
+            '-q', 'small',
+            '--', pwd+'./test.sh', '{:.1f}'.format(au)]
+    subprocess.run(rc)
