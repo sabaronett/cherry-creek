5eab6f7e30cf6f08ae70ca866b6b943871da2f8d
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-14 15:19:42 -0700

    Call args.py with args

--- reboundx/implementation/test/test.sh
+++ reboundx/implementation/test/test.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
-#PBS -j oe
-#PBS -l select=1:ncpus=1:mem=1gb
-#PBS -l walltime=00:01:00
-#PBS -m abe
-#PBS -M barons2@unlv.nevada.edu
-#PBS -N test_$1
-#PBS -q small
 
-cd $PBS_O_WORKDIR
 module load conda
 conda activate rebx-3.4.1
-echo Test no. $1
+python args.py $1
