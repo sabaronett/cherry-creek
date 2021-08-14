9c08b3756bee0b8bd7a96d94bb3a0b331657b62d
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2021-08-14 15:22:54 -0700

    Iteratively call shell script

--- reboundx/implementation/test/multipbs.py
+++ reboundx/implementation/test/multipbs.py
@@ -1,7 +1,7 @@
 import subprocess
 import numpy as np
 
-pwd = '/home/barons2/github/sabaronett/cherry-creek/reboundx/implementation/test'
+# pwd = '/home/barons2/github/sabaronett/cherry-creek/reboundx/implementation/test'
 aus = np.arange(1, 1.2, 0.1) # in AU
 
 with open('processes.txt', 'w') as f:
@@ -14,8 +14,7 @@ with open('processes.txt', 'w') as f:
               '-M', 'barons2@unlv.nevada.edu',
               '-N', 'test_{:.1f}au'.format(au),
               '-q', 'small',
-              '-S', '/home/barons2/.conda/envs/rebx-3.4.1/bin/python',
-              pwd+'/args.py', '{:.1f}'.format(au)]
+              '--', './test.sh', '{:.1f}'.format(au)]
         complete = subprocess.run(rc)
         f.write('args = %s\n'%(complete.args))
         f.write('  returncode = %s\n'%(complete.returncode))
